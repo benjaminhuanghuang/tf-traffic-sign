00166d568ce0a974d8e291728b33fb68476f5e37
Benjamin Huang <bhuang@rms.com>
2018-03-16 16:53:47 -0700

    update

--- tf_traffic_sign.py
+++ tf_traffic_sign.py
@@ -77,3 +77,101 @@ display_images_and_labels(images, labels)
 # Display all speed sign
 display_label_images(images, 32)
 
+# Adjust the image size
+images32 = [skimage.transform.resize(image, (32, 32)) for image in images]
+display_images_and_labels(images32, labels)
+
+
+# Create a graph to hold the model.
+graph = tf.Graph()
+
+# Create model in the graph.
+with graph.as_default():
+    # Placeholders for inputs and labels.
+    images_ph = tf.placeholder(tf.float32, [None, 32, 32, 3])
+    labels_ph = tf.placeholder(tf.int32, [None])
+
+    # Flatten input from: [None, height, width, channels]
+    # To: [None, height * width * channels] == [None, 3072]
+    images_flat = tf.contrib.layers.flatten(images_ph)
+
+    # Fully connected layer. 
+    # Generates logits of size [None, 62]
+    logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+
+    # Convert logits to label indexes (int).
+    # Shape [None], which is a 1D vector of length == batch_size.
+    predicted_labels = tf.argmax(logits, 1)
+
+    # Define the loss function. 
+    # Cross-entropy is a good choice for classification.
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits, labels_ph))
+
+    # Create training op.
+    train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+    # And, finally, an initialization op to execute before training.
+    # TODO: rename to tf.global_variables_initializer() on TF 0.12.
+    init = tf.initialize_all_variables()
+
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", predicted_labels)
+
+
+# Create a session to run the graph we created.
+session = tf.Session(graph=graph)
+
+# First step is always to initialize all variables. 
+# We don't care about the return value, though. It's None.
+_ = session.run([init])
+
+for i in range(201):
+    _, loss_value = session.run([train, loss], 
+                                feed_dict={images_ph: images_a, labels_ph: labels_a})
+    if i % 10 == 0:
+        print("Loss: ", loss_value)
+
+# Pick 10 random images
+sample_indexes = random.sample(range(len(images32)), 10)
+sample_images = [images32[i] for i in sample_indexes]
+sample_labels = [labels[i] for i in sample_indexes]
+
+# Run the "predicted_labels" op.
+predicted = session.run([predicted_labels], 
+                        feed_dict={images_ph: sample_images})[0]
+print(sample_labels)
+print(predicted)
+
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = sample_labels[i]
+    prediction = predicted[i]
+    plt.subplot(5, 2,1+i)
+    plt.axis('off')
+    color='green' if truth == prediction else 'red'
+    plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction), 
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i])
+
+#  Evaluation
+# Load the test dataset.
+test_images, test_labels = load_data(test_data_dir)
+# Transform the images, just like we did with the training set.
+test_images32 = [skimage.transform.resize(image, (32, 32))
+                 for image in test_images]
+display_images_and_labels(test_images32, test_labels)
+# Run predictions against the full test set.
+predicted = session.run([predicted_labels], 
+                        feed_dict={images_ph: test_images32})[0]
+# Calculate how many matches we got.
+match_count = sum([int(y == y_) for y, y_ in zip(test_labels, predicted)])
+accuracy = match_count / len(test_labels)
+print("Accuracy: {:.3f}".format(accuracy))
+
+# Close the session. This will destroy the trained model.
+session.close()
