4926f7d3ba341357dfd1f4be298e748690f69e35
Benjamin Huang <bhuang@rms.com>
2018-07-09 21:55:43 -0700

    update

--- tf_traffic_sign.py
+++ tf_traffic_sign.py
@@ -1,89 +1,41 @@
 import os
 import random
-
-
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-
-import skimage.data
-import skimage.transform
+import matplotlib.pyplot as plt 
+from skimage import transform
+from skimage import data
+from skimage.color import rgb2gray
+import tensorflow as tf 
 import numpy as np
-import tensorflow as tf
 
-def load_data(data_dir):
-    """
-    Loads a data set and returns two lists:
-    
-    images: a list of Numpy arrays, each representing an image.
-    labels: a list of numbers that represent the images labels.
-    """
-    # Get all subdirectories of data_dir. Each represents a label.
-    directories = [d for d in os.listdir(data_dir) 
-                   if os.path.isdir(os.path.join(data_dir, d))]
-    # Loop through the label directories and collect the data in
-    # two lists, labels and images.
-    labels = []
-    images = []
-    for d in directories:
-        label_dir = os.path.join(data_dir, d)
-        file_names = [os.path.join(label_dir, f) 
-                      for f in os.listdir(label_dir) if f.endswith(".ppm")]
-        # For each label, load it's images and add them to the images list.
-        # And add the label number (i.e. directory name) to the labels list.
-        for f in file_names:
-            images.append(skimage.data.imread(f))
-            labels.append(int(d))    # dir name is the label of the image, int 0 to 61
-    return images, labels
+from utils import load_data, display_images_and_labels
 
-def display_images_and_labels(images, labels):
-    """Display the first image of each label."""
-    unique_labels = set(labels)
-    plt.figure(figsize=(15, 15))
-    i = 1
-    for label in unique_labels:
-        # Pick the first image for each label.
-        image = images[labels.index(label)]
-        plt.subplot(8, 8, i)  # A grid of 8 rows x 8 columns
-        plt.axis('off')
-        plt.title("Label {0} ({1})".format(label, labels.count(label)))
-        i += 1
-        _ = plt.imshow(image)
-    plt.show()
-
-def display_label_images(images, label):
-    """Display images of a specific label."""
-    limit = 24  # show a max of 24 images
-    plt.figure(figsize=(15, 5))
-    i = 1
-
-    start = labels.index(label)
-    end = start + labels.count(label)
-    for image in images[start:end][:limit]:
-        plt.subplot(3, 8, i)  # 3 rows, 8 per row
-        plt.axis('off')
-        i += 1
-        plt.imshow(image)
-    plt.show()
-
-#--------------------------------------------------------
-#  Start running
-#--------------------------------------------------------
 # Load training and testing datasets.
 ROOT_PATH = "./data"
 train_data_dir = os.path.join(ROOT_PATH, "Training")
 test_data_dir = os.path.join(ROOT_PATH, "Testing")
 
-# get images and labels (0 to 61)
+# get images and labels (0 to 61) as list
 images, labels = load_data(train_data_dir)
-display_images_and_labels(images, labels)
 
-# Display all speed sign
-display_label_images(images, 32)
+# Resize images
+images32 = [transform.resize(image, (28, 28)) for image in images]
+images32 = rgb2gray(np.array(images32))
 
-# Adjust the image size
-images32 = [skimage.transform.resize(image, (32, 32)) for image in images]
-display_images_and_labels(images32, labels)
+x = tf.placeholder(dtype = tf.float32, shape = [None, 28, 28])
+y = tf.placeholder(dtype = tf.int32, shape = [None])
+images_flat = tf.contrib.layers.flatten(x)
+logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = logits))
+train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+correct_pred = tf.argmax(logits, 1)
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", correct_pred)
 
 
 # Create a graph to hold the model.
